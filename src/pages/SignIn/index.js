8d137db8d0dcc5b6d9cce027a72381a97b67286c
MarcosVel <marcos.veloso@sempreceub.com>
2021-09-25 16:32:25 -0300

    created login view

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Keyboard } from 'react-native';
+import {
+  TouchableWithoutFeedback,
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from './styles';
 
 export default function SignIn() {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+      <Background>
+        <Container>
+          <Logo source={ require('../../assets/Logo.png') } />
+
+          <AreaInput>
+            <Input
+              placeholder='E-mail'
+              autoCorrect={ false }
+              autoCapitalize='none' // não começar com maiúscula
+              value={ email }
+              onChangeText={ (text) => setEmail(text) }
+            />
+          </AreaInput>
+
+          <AreaInput>
+            <Input
+              placeholder='Senha'
+              autoCorrect={ false }
+              autoCapitalize='none'
+              value={ password }
+              onChangeText={ (text) => setPassword(text) }
+            />
+          </AreaInput>
+
+          <SubmitButton>
+            <SubmitText>Acessar</SubmitText>
+          </SubmitButton>
+
+          <Link>
+            <LinkText>Criar uma conta!</LinkText>
+          </Link>
+
+        </Container>
+      </Background>
+    </TouchableWithoutFeedback>
   )
 }
-
-const styles = StyleSheet.create({})
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components/native';
+import { COLORS } from '../../../styles';
+
+export const TouchableWithoutFeedback = styled.TouchableWithoutFeedback`
+`;
+
+export const Background = styled.View`
+  flex: 1;
+  background-color: ${ COLORS.black };
+`;
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  margin-bottom: 40px;
+`;
+
+export const AreaInput = styled.View`
+  flex-direction: row;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: `${ COLORS.placeholder }`
+})`
+  background: ${ COLORS.blackSeven };
+  width: 90%;
+  font-size: 17px;
+  color: ${ COLORS.white };
+  margin-bottom: 15px;
+  padding: 10px 15px;
+  border-radius: 8px;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  background-color: ${ COLORS.green };
+  width: 90%;
+  height: 45px;
+  border-radius: 8px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const SubmitText = styled.Text`
+  font-size:20px;
+  color: ${ COLORS.black };
+`;
+
+export const Link = styled.TouchableOpacity`
+`;
+
+export const LinkText = styled.Text`
+  color: ${ COLORS.white }
+`;
