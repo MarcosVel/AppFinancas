1aa18c2937b1795353e9008cba37746c6214fc4e
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-25 02:42:33 -0300

    refactor: adjusting eslint and prettier of code

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { COLORS } from '../../styles';
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { COLORS } from "../../styles";
 
-import Home from '../pages/Home';
-import New from '../pages/New';
-import Profile from '../pages/Profile';
+import Home from "../pages/Home";
+import New from "../pages/New";
+import Profile from "../pages/Profile";
 
 const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
   return (
     <AppDrawer.Navigator
-      screenOptions={ {
+      screenOptions={{
         drawerStyle: {
-          backgroundColor: `${ COLORS.black }`,
+          backgroundColor: `${COLORS.black}`,
         },
         drawerLabelStyle: {
-          fontWeight: 'bold',
+          fontWeight: "bold",
         },
-        drawerActiveTintColor: `${ COLORS.white }`,
-        drawerActiveBackgroundColor: `${ COLORS.green }`,
-        drawerInactiveTintColor: `${ COLORS.placeholder }`,
-        drawerInactiveBackgroundColor: `${ COLORS.black }`,
+        drawerActiveTintColor: `${COLORS.white}`,
+        drawerActiveBackgroundColor: `${COLORS.green}`,
+        drawerInactiveTintColor: `${COLORS.placeholder}`,
+        drawerInactiveBackgroundColor: `${COLORS.black}`,
         drawerItemStyle: {
-          marginVertical: 5
-        }
-      } }
+          marginVertical: 5,
+        },
+      }}
     >
-      <AppDrawer.Screen name='Home' component={ Home }
-      // options={ { headerShown: false } } 
+      <AppDrawer.Screen
+        name="Home"
+        component={Home}
+        // options={ { headerShown: false } }
       />
-      <AppDrawer.Screen name='Registrar' component={ New }
-      // options={ { headerShown: false } } 
+      <AppDrawer.Screen
+        name="Registrar"
+        component={New}
+        // options={ { headerShown: false } }
       />
-      <AppDrawer.Screen name='Perfil' component={ Profile }
-      options={ { headerShown: false } } 
+      <AppDrawer.Screen
+        name="Perfil"
+        component={Profile}
+        options={{ headerShown: false }}
       />
     </AppDrawer.Navigator>
-  )
+  );
 }
 
 export default AppRoutes;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,22 @@
-import React, { useContext } from 'react';
-import AuthRoutes from './auth.routes';
-import AppRoutes from './app.routes';
-import { AuthContext } from '../contexts/auth';
-import { ActivityIndicator, View } from 'react-native';
-import { COLORS } from '../../styles';
+import { useContext } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { COLORS } from "../../styles";
+import { AuthContext } from "../contexts/auth";
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
 
 function Routes() {
   const { signed, loading } = useContext(AuthContext);
 
   if (loading) {
     return (
-      <View style={ { flex: 1, justifyContent: 'center', alignItems: 'center' } }>
-        <ActivityIndicator size="large" color={ COLORS.green } />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color={COLORS.green} />
       </View>
-    )
+    );
   }
 
-  return (
-    signed ? <AppRoutes /> : <AuthRoutes />
-  )
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 }
 
 export default Routes;
