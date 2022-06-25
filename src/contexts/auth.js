ada1416d586c73bf922660e50fa877c1aa56bb0f
MarcosVel <marcos.veloso@sempreceub.com>
2022-06-25 02:43:31 -0300

    feat: refactoring code and add loading to buttons

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,16 +1,17 @@
-import React, { createContext, useEffect, useState } from 'react';
-import firebase from '../services/firebaseConnection';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useEffect, useState } from "react";
+import firebase from "../services/firebaseConnection";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
-  const [ user, setUser ] = useState(null);
-  const [ loading, setLoading ] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [loadingReq, setLoadingReq] = useState(false);
 
   useEffect(() => {
     async function loadStorage() {
-      const storageUser = await AsyncStorage.getItem('Auth_user');
+      const storageUser = await AsyncStorage.getItem("Auth_user");
 
       if (storageUser) {
         setUser(JSON.parse(storageUser));
@@ -21,17 +22,24 @@ function AuthProvider({ children }) {
     }
 
     loadStorage();
-  }, [])
+  }, []);
 
   // cadastrar usuário
   async function signUp(email, password, nome) {
-    await firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(async (value) => {
+    setLoadingReq(true);
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async value => {
         let uid = value.user.uid;
-        await firebase.database().ref('users').child(uid).set({
-          saldo: 0,
-          nome: nome,
-        })
+        await firebase
+          .database()
+          .ref("users")
+          .child(uid)
+          .set({
+            saldo: 0,
+            nome: nome,
+          })
           .then(() => {
             let data = {
               uid: uid,
@@ -41,20 +49,29 @@ function AuthProvider({ children }) {
 
             setUser(data);
             storageUser(data); // não ficar deslogando após entrar
-          })
+            setLoadingReq(false);
+          });
       })
-      .catch((error) => {
+      .catch(error => {
+        setLoadingReq(false);
         alert(error.code);
-      })
+      });
   }
 
   // login
   async function signIn(email, password) {
-    await firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(async (value) => {
+    setLoadingReq(true);
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async value => {
         let uid = value.user.uid;
-        await firebase.database().ref('users').child(uid).once('value')
-          .then((snapshot) => {
+        await firebase
+          .database()
+          .ref("users")
+          .child(uid)
+          .once("value")
+          .then(snapshot => {
             let data = {
               uid: uid,
               nome: snapshot.val().nome,
@@ -63,29 +80,40 @@ function AuthProvider({ children }) {
 
             setUser(data);
             storageUser(data); // não ficar deslogando após entrar
-          })
+            setLoadingReq(false);
+          });
       })
-      .catch((error) => {
+      .catch(error => {
+        setLoadingReq(false);
         alert(error.code);
-      })
+      });
   }
 
   async function storageUser(data) {
-    await AsyncStorage.setItem('Auth_user', JSON.stringify(data)); // não pode passar objeto, só string, então transforma o data
+    await AsyncStorage.setItem("Auth_user", JSON.stringify(data)); // não pode passar objeto, só string, então transforma o data
   }
 
   async function signOut() {
     await firebase.auth().signOut();
-    await AsyncStorage.clear()
-      .then(() => {
-        setUser(null);
-      })
+    await AsyncStorage.clear().then(() => {
+      setUser(null);
+    });
   }
 
   return (
     // !!user converte o null em boolean, se não tiver usuário = false, se tiver = true
-    <AuthContext.Provider value={ { signed: !!user, user, signUp, signIn, loading, signOut } }>
-      { children }
+    <AuthContext.Provider
+      value={{
+        signed: !!user,
+        user,
+        signUp,
+        signIn,
+        loading,
+        signOut,
+        loadingReq,
+      }}
+    >
+      {children}
     </AuthContext.Provider>
   );
 }
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,67 +1,69 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useContext, useState } from 'react'
-import { Keyboard } from 'react-native';
-import { AuthContext } from '../../contexts/auth';
+import { useNavigation } from "@react-navigation/native";
+import { useContext, useState } from "react";
+import { ActivityIndicator, Keyboard, Platform } from "react-native";
+import { AuthContext } from "../../contexts/auth";
 import {
-  TouchableWithoutFeedback,
+  AreaInput,
   Background,
   Container,
-  Logo,
-  AreaInput,
   Input,
-  SubmitButton,
-  SubmitText,
   Link,
   LinkText,
-} from './styles';
+  Logo,
+  SubmitButton,
+  SubmitText,
+  TouchableWithoutFeedback,
+} from "./styles";
 
 export default function SignIn() {
-  const [ email, setEmail ] = useState('')
-  const [ password, setPassword ] = useState('')
-
   const navigation = useNavigation();
-  const { signIn } = useContext(AuthContext);
+  const { signIn, loadingReq } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function handleLogin() {
     signIn(email, password);
   }
 
   return (
-    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <Background>
-        <Container>
-          <Logo source={ require('../../assets/Logo.png') } />
+        <Container behavior={Platform.OS === "ios" ? "padding" : ""} enabled>
+          <Logo source={require("../../assets/Logo.png")} />
 
           <AreaInput>
             <Input
-              placeholder='E-mail'
-              autoCorrect={ false }
-              autoCapitalize='none' // não começar com maiúscula
-              value={ email }
-              onChangeText={ (text) => setEmail(text) }
+              placeholder="E-mail"
+              autoCorrect={false}
+              autoCapitalize="none" // não começar com maiúscula
+              value={email}
+              onChangeText={text => setEmail(text)}
             />
           </AreaInput>
 
           <AreaInput>
             <Input
-              placeholder='Senha'
-              autoCorrect={ false }
-              autoCapitalize='none'
-              value={ password }
-              onChangeText={ (text) => setPassword(text) }
+              placeholder="Senha"
+              autoCorrect={false}
+              autoCapitalize="none"
+              value={password}
+              onChangeText={text => setPassword(text)}
             />
           </AreaInput>
 
-          <SubmitButton onPress={ handleLogin }>
-            <SubmitText>Acessar</SubmitText>
+          <SubmitButton onPress={handleLogin}>
+            {loadingReq ? (
+              <ActivityIndicator size={24} color="#fff" />
+            ) : (
+              <SubmitText>Acessar</SubmitText>
+            )}
           </SubmitButton>
 
-          <Link onPress={ () => navigation.navigate('SignUp') }>
+          <Link onPress={() => navigation.navigate("SignUp")}>
             <LinkText>Criar uma conta</LinkText>
           </Link>
-
         </Container>
       </Background>
     </TouchableWithoutFeedback>
-  )
+  );
 }
